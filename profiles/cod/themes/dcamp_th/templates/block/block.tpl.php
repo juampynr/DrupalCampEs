893f1a0590371c02271569b9439c1be13001fd33
casanova <casanova347@gmail.com>
2015-08-20 15:02:42 -0300

    - Agrego video de home (modulo y ajustes). - Ajustes en maquetación menu principal.

--- profiles/cod/themes/dcamp_th/templates/block/block.tpl.php
+++ profiles/cod/themes/dcamp_th/templates/block/block.tpl.php
@@ -45,14 +45,12 @@
  */
 ?>
 <div<?php print $attributes; ?>>
-  <div class="l-container">
-    <?php print render($title_prefix); ?>
-    <?php if ($block->subject): ?>
-      <h2<?php print $title_attributes; ?>><?php print $block->subject; ?></h2>
-    <?php endif; ?>
-    <?php print render($title_suffix); ?>
-    <div<?php print $content_attributes; ?>>
-      <?php print $content; ?>
-    </div>
+  <?php print render($title_prefix); ?>
+  <?php if ($block->subject): ?>
+    <h2<?php print $title_attributes; ?>><?php print $block->subject; ?></h2>
+  <?php endif; ?>
+  <?php print render($title_suffix); ?>
+  <div<?php print $content_attributes; ?>>
+    <?php print $content; ?>
   </div>
 </div>

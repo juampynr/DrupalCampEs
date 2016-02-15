bfd0812d30b4bde2fd95d0ecaa6e97efb43de159
capynet <capy.net@gmail.com>
2016-02-15 15:54:37 +0100

    Fix settings.

--- sites/default/settings.php
+++ sites/default/settings.php
@@ -217,9 +217,9 @@ $databases = array (
   array (
     'default' =>
     array (
-      'database' => 'dcamp',
-      'username' => 'root',
-      'password' => 'capy',
+      'database' => '2016camp',
+      'username' => '2016camp',
+      'password' => '2016camp',
       'host' => 'localhost',
       'port' => '',
       'driver' => 'mysql',
